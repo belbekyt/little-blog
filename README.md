19c39dac3cdd9519220407f68a8d813ea84097fe
Miłosz Pawłowski <milosz.pawlowski.torun@wp.pl>
2023-02-11 19:53:42 +0100

    Created project structure
    created project structure and do main site in 50%

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # little-blog
  Little blog app with PHP & MySQL
  
- USING: HTML, CSS, SCSS, PHP, MySQL
+ Using: HTML, CSS, SCSS, PHP, MySQL
  
- STATUS: IN PROGRESS...
+ Status: IN PROGRESS...
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Little Blog</title>
+
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles link -- file compiled by scss -->
+    <link rel="stylesheet" href="./css/styles.css"/>
+</head>
+<body>
+    <header>
+        <h1>Little blog with Posts</h2>
+    </header>
+    <section class="banner">
+        <div>
+            <img src="./images/header.png" alt="3D tree image">
+        </div>
+        <div class="banner-content">
+            <h2>Help us create more posts!</h2>
+            <p>Our app likes new posts - so it's your mission to upload new posts to our website and we will show them on the main site. Be careful, we are watching what you are posting. Let the fun begin!! Share your emotions or moment now.</p>
+            <a href="./sites/posts.php"><button>Create Post!</button></a>
+        </div>
+    </section>
+    <section>
+
+    </section>
+    <footer>
+        <p>Done by Miłosz Pawłowski - Web Developer</p>
+    </footer>
+</body>
+</html>
